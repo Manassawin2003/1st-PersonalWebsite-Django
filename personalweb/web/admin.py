16117d8bdbc130c2653c83f5b622c686d16d3aa7
Manassawin2003 <150880489+Manassawin2003@users.noreply.github.com>
2024-08-20 17:28:23 +0700

    Update
    20/8/2024
    Addnewfield by using admin page

--- personalweb/web/admin.py
+++ personalweb/web/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from .models import Profile, Addnewfield
 
-from.models import Profile
+class ProfileFieldInline(admin.TabularInline):
+    model = Addnewfield
+    extra = 1
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'location')
+    search_fields = ('name', 'email')
+    inlines = [ProfileFieldInline]
+
+@admin.register(Addnewfield)
+class ProfileFieldAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'key')
--- personalweb/web/models.py
+++ personalweb/web/models.py
@@ -1,5 +1,5 @@
-from django.db import models  # Make sure to import models
-from django.contrib.auth.models import User  # Import User model if you're using it
+from django.db import models
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -8,9 +8,16 @@ class Profile(models.Model):
     email = models.EmailField()
     location = models.CharField(max_length=255)
     story = models.TextField()
-    interestspecial = models.TextField()
     projectdevelop = models.TextField()
     tooldevelop = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
+class Addnewfield(models.Model):
+    profile = models.ForeignKey(Profile, related_name='fields', on_delete=models.CASCADE)
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+
+    def __str__(self):
+        return f"{self.key}"
